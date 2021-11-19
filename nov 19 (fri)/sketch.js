8d6a4df910171f07102af1877882f49b1331e7c8
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-11-19 12:09:34 -0600

    Update sketch.js

--- nov 19 (fri)/sketch.js
+++ nov 19 (fri)/sketch.js
@@ -1,15 +1,72 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Working with Arrays and loops
+// Srushti Vyas
+// Nov 19th, 2021
+// A quick refresher and application of for loops and arrays
+
+// global variable
+//let names = ["Srushti Vyas", "Aula", "yug", "krupali", "BTS" ];
+//                1          2         3       4         5
+//let emptyArray = [];
+
+let x = [];
+let y = [];
+const NUM_POINTS = 20;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  intArrays();
 }
+
+function intArrays(){
+  // we will use this function to set up all our initial x,y values
+  for(let i = 0; i < NUM_POINTS; i++){
+    x.push(random(0,width));
+    y.push(random(0,height));
+  }
+}
+
+function drawArrays(){
+  //read through the parallel arrays, and draw some circles + connecting lines
+  for(let i = 0; i < x.length; i++){
+    fill(0);
+    circle(x[i], y[i], 10);
+    //only draw a line line forward if one exists
+    if(i < x.length - 1){
+      line(x[i],y[i], x[i+1], y[i+1]);
+    }
+  }
+}
+
+function updateArrays(){
+  //push one new value on, and delete first value
+  x.push(random(0, width));
+  y.push(random(0, height));
+  x.shift();
+  y.shift();
+}
+
+
+
 
 function draw() {
   background(220);
+  drawArrays();
+  updateArrays();
 }
+
+
+
+// function doloops(){
+  // first: While loop
+
+// let counter = 0;
+// while(counter < 6){
+//   print(namess[counter]);
+//   counter ++;
+// }
+
+// second: For Loops (basic)   (later,enhanced)
+// for(let i = 0; i < names.length; i++){
+//   print(names[1]);
+// }
