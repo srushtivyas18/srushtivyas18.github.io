19bfba8a9180c0119897371305773cfb977e87f2
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-11-22 12:07:14 -0600

    drawing loops example

--- 05 drawing with loops/sketch.js
+++ 05 drawing with loops/sketch.js
@@ -1,15 +1,26 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Drawing with loops
+// Srushti Vyas
+// Nov 22, 2021
+// using loops to draw more complex images
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  loopOne();
+
+  //at the bottom of the loop, a single new image is drawn
+}
+
+function loopOne(){
+  //use a loop to draw some shapes
+  let spacing = map(mouseX,0,width,10,100);    // can be 0
+  for(let x = 0; x < width; x += spacing){
+    // map a fill value
+    let fillValue = map(x,0,width,0,255);
+    fill(fillValue);
+    circle(x, height/2, spacing);
+  }
 }
