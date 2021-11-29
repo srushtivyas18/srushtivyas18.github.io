5f969e1e6decb18124a45f115d9e3686d2ada6bd
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-11-29 11:10:54 -0600

    Update sketch.js

--- Refactor  Three/sketch.js
+++ Refactor  Three/sketch.js
@@ -1,22 +1,16 @@
 // //Black and White Target
+//Srushti Vyas
+//Making a target as a new function
 function setup() {
   createCanvas(400, 400);
   }
   function draw() {
   background(240);
-  // ellipse(200, 200, 400, 400);
-  // ellipse(200, 200, 360, 360);
-  // ellipse(200, 200, 320, 320);
-  // ellipse(200, 200, 280, 280);
-  // ellipse(200, 200, 240, 240);
-  // ellipse(200, 200, 200, 200);
-  // ellipse(200, 200, 160, 160);
-  // ellipse(200, 200, 120, 120);
-  // ellipse(200, 200, 80, 80);
-  // ellipse(200, 200, 40, 40);
     target();
   }
-  
+
+
+  // d = diameter
   function target(){
     for(let d = 400; d >= 40; d-= 40){
       circle(200,200,d);
