84bd7cea0265cbe374e4ce40edf0db638253715e
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-11-30 11:41:10 -0600

    Update sketch.js

--- Refactoring four/sketch.js
+++ Refactoring four/sketch.js
@@ -1,137 +1,28 @@
 ////Chess Board
+
+
+let currentFill = 255;
 function setup() {
-createCanvas(600, 600);
+    createCanvas(600, 600);
 }
 function draw() {
-fill(255);
-rect(0,0,75,75);
-fill(0);
-rect(0,75,75,75);
-fill(255);
-rect(0,150,75,75);
-fill(0);
-rect(0,225,75,75);
-fill(255);
-rect(0,300,75,75);
-fill(0);
-rect(0,375,75,75);
-fill(255);
-rect(0,450,75,75);
-fill(0);
-rect(0,525,75,75);
-fill(0);
-rect(75,0,75,75);
-fill(255);
-rect(75,75,75,75);
-fill(0);
-rect(75,150,75,75);
-fill(255);
-rect(75,225,75,75);
-fill(0);
-rect(75,300,75,75);
-fill(255);
-rect(75,375,75,75);
-fill(0);
-rect(75,450,75,75);
-fill(255);
-rect(75,525,75,75);
-fill(255);
-rect(150,0,75,75);
-fill(0);
-rect(150,75,75,75);
-fill(255);
 
-rect(150,150,75,75);
-fill(0);
-rect(150,225,75,75);
-fill(255);
-rect(150,300,75,75);
-fill(0);
-rect(150,375,75,75);
-fill(255);
-rect(150,450,75,75);
-fill(0);
-rect(150,525,75,75);
-fill(0);
-rect(225,0,75,75);
-fill(255);
-rect(225,75,75,75);
-fill(0);
-rect(225,150,75,75);
-fill(255);
-rect(225,225,75,75);
-fill(0);
-rect(225,300,75,75);
-fill(255);
-rect(225,375,75,75);
-fill(0);
-rect(225,450,75,75);
-fill(255);
-rect(225,525,75,75);
-fill(255);
-rect(300,0,75,75);
-fill(0);
-rect(300,75,75,75);
-fill(255);
-rect(300,150,75,75);
-fill(0);
-rect(300,225,75,75);
-fill(255);
-rect(300,300,75,75);
-fill(0);
-rect(300,375,75,75);
-fill(255);
-rect(300,450,75,75);
-fill(0);
-rect(300,525,75,75);
-fill(0);
+    chessBoard();
+}
 
-rect(375,0,75,75);
-fill(255);
-rect(375,75,75,75);
-fill(0);
-rect(375,150,75,75);
-fill(255);
-rect(375,225,75,75);
-fill(0);
-rect(375,300,75,75);
-fill(255);
-rect(375,375,75,75);
-fill(0);
-rect(375,450,75,75);
-fill(255);
-rect(375,525,75,75);
-fill(255);
-rect(450,0,75,75);
-fill(0);
-rect(450,75,75,75);
-fill(255);
-rect(450,150,75,75);
-fill(0);
-rect(450,225,75,75);
-fill(255);
-rect(450,300,75,75);
-fill(0);
-rect(450,375,75,75);
-fill(255);
-rect(450,450,75,75);
-fill(0);
-rect(450,525,75,75);
-fill(0);
-rect(525,0,75,75);
-fill(255);
-rect(525,75,75,75);
-fill(0);
-rect(525,150,75,75);
-fill(255);
-rect(525,225,75,75);
-fill(0);
-rect(525,300,75,75);
-fill(255);
-rect(525,375,75,75);
-fill(0);
-
-rect(525,450,75,75);
-fill(255);
-rect(525,525,75,75);
+function chessBoard() {
+    for (x = 0; x < 525; x += 75) {
+        for (y = 0; y < 525; y += 75) {
+            square(x, y, 75);
+            if (currentFill === 255) {
+                fill(255);
+                currentFill = 0;
+            }
+            else {
+                fill(0);
+                currentFill = 255;
+            }
+        }
+    }
+    ffg
 }
