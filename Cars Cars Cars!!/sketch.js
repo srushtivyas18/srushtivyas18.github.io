a222d0834717021ab80eb8219ea1c10cda5bd919
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-12-03 10:56:35 -0600

    Update sketch.js

--- Cars Cars Cars!!/sketch.js
+++ Cars Cars Cars!!/sketch.js
@@ -1,9 +1,8 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Cars Cars Cars!!!
+// Srushti Vyas
+// Dec 3rd, 2021
+// Making a car simulation
+
 
 
 function setup() {
@@ -12,4 +11,17 @@ function setup() {
 
 function draw() {
   background(220);
+  drawRoad();
+}
+
+function drawRoad(){
+
+  fill(0);
+  rect(0,height/4,width,height/2);
+  for(let i = 0; i < width; i++){
+    fill(255);
+    rect(0,height/2.1,i,height/100);
+  }
+  
+
 }
