5cbb35fac207865b5320eae4297d81324fdd4e36
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-12-06 12:09:48 -0600

    Update sketch.js

--- Cars Cars Cars!!/sketch.js
+++ Cars Cars Cars!!/sketch.js
@@ -3,7 +3,7 @@
 // Dec 3rd, 2021
 // Making a car simulation
 
-
+let eastBound = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +12,10 @@ function setup() {
 function draw() {
   background(220);
   drawRoad();
+}
+
+function mouseClicked(){
+  eastbound.pus(new Vehicle)
 }
 
 function drawRoad(){
@@ -24,4 +28,52 @@ function drawRoad(){
     rect(i*(width+250)/16,height/2.1,width/16,height/100);
   }
 
+}
+
+class Vehicle{
+  constructor(x,y,dir){
+    this.x = x;
+    this.y = y;
+    this.dir = dir;
+    this.c = color(random(255),random(255),random(255));
+    this.type = int(random(2));
+
+  }
+
+
+  move(){
+    
+  }
+
+  display(){
+    if(this.type === 0){// car
+      this.drawCar();
+    }
+    else if (this.type === 1){//truck
+      this.drawTruck();
+    }
+  }
+
+  drawCar(){
+    fill(this.c);
+    rect(this.x, this.y, 100, 40);
+  }
+
+  drawTruck(){
+    fill(this.c);
+    ellipse(this.x,this.y,100,40);
+  }
+
+  speedUp(){
+
+  }
+  speedDown(){
+
+  }
+  changeColor(){
+
+  }
+  action(){
+    this.display();
+  }
 }
