58cc5ead56ef29bac825e2033f903983dc37a435
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-12-09 10:53:21 -0600

    refactor 4

--- Refactoring four/sketch.js
+++ Refactoring four/sketch.js
@@ -1,9 +1,10 @@
 ////Chess Board
 
 
-let currentFill = 255;
+let currentFill = 0;
 function setup() {
     createCanvas(600, 600);
+    noLoop();
 }
 function draw() {
 
@@ -13,7 +14,7 @@ function draw() {
 function chessBoard() {
     for (x = 0; x < 525; x += 75) {
         for (y = 0; y < 525; y += 75) {
-            square(x, y, 75);
+            
             if (currentFill === 255) {
                 fill(255);
                 currentFill = 0;
@@ -22,7 +23,8 @@ function chessBoard() {
                 fill(0);
                 currentFill = 255;
             }
+            square(x, y, 75);
         }
     }
-    ffg
+
 }
