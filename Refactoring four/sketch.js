e34f269b1ea1f7f8397cf4ff73944975581c4cb4
srushtivyas18 <94391746+srushtivyas18@users.noreply.github.com>
2021-12-09 11:05:19 -0600

    refactor 4

--- Refactoring four/sketch.js
+++ Refactoring four/sketch.js
@@ -1,10 +1,13 @@
-////Chess Board
+//Chess Board
+// Refactor four.
+// Srushti Vyas
 
 
-let currentFill = 0;
+let currentFill = 0; // Made a fill varibale, to allow us to change colors of the squares 
+
 function setup() {
     createCanvas(600, 600);
-    noLoop();
+    noLoop();  // No loop function so it won;t blink again and again
 }
 function draw() {
 
@@ -12,18 +15,20 @@ function draw() {
 }
 
 function chessBoard() {
+    // draws the pattern of the suare from the provided numbers
+    // using two for loops for for width squares and the heigth squares......(horizontal squares row and vertical squares row)
     for (x = 0; x < 525; x += 75) {
         for (y = 0; y < 525; y += 75) {
             
             if (currentFill === 255) {
-                fill(255);
+                fill(255);                    // if this is true then it will make the black squares
                 currentFill = 0;
             }
             else {
                 fill(0);
-                currentFill = 255;
+                currentFill = 255;           // or else it will make the white ones.
             }
-            square(x, y, 75);
+            square(x, y, 75); 
         }
     }
 
